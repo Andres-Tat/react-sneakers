084a70cae82e9e2f8b7ba47d590214b3c5a3f039
Andrey Tatarnikov <andrestat95@mail.ru>
2023-05-29 22:16:06 +0300

    add basket

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
-
 function App() {
   return (
     <div className="wrapper clear">
       <div className="overlay">
         <div className="drawer">
-          <h2>Корзина</h2>
-          <div className="cartItem">
-            <img src="/img/sneakers/1.jpg" alt="Sneakers" ></img>
-            <div>
-              <p>Мужские Кроссовки Nike Blazer Mid Suede</p>
-              <b>12 999 руб.</b>
+          <h2 className="mb-30">Корзина</h2>
+          
+          <div className="items">
+            <div className="cartItem d-flex align-center mb-20">
+              <div style={{ backgroundImage: 'url(/img/sneakers/1.jpg)'}} className="cartItemImg"></div>
+                <div className="mr-20 flex">
+                  <p className="mb-5">Мужские Кроссовки Nike Blazer Mid Suede</p>
+                  <b>12 999 руб.</b>
+                </div>
+              <img className="removeBtn" src="/img/sneakers/btn-remove.svg" alt="Remove" />
+            </div>
+            <div className="cartItem d-flex align-center">
+              <div style={{ backgroundImage: 'url(/img/sneakers/1.jpg)'}} className="cartItemImg"></div>
+                <div className="mr-20 flex">
+                  <p className="mb-5">Мужские Кроссовки Nike Blazer Mid Suede</p>
+                  <b>12 999 руб.</b>
+                </div>
+              <img className="removeBtn" src="/img/sneakers/btn-remove.svg" alt="Remove" />
             </div>
           </div>
+
+            <ul className="cartTotaBlock">
+              <li>
+                <span>Итого:</span>
+                <div></div>
+                <b>21 498 руб. </b>
+              </li>
+              <li>
+                <span>Налог 5%:</span>
+                <div></div>
+                <b>1074 руб. </b>
+              </li>
+            </ul>
         </div>
       </div>
+
       <header className="d-flex justify-between align-center p-40">
         <div className="d-flex align-center">
           <img width={40} height={40} src="/img/logo.png" />
