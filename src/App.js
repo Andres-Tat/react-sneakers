9c8b11c75fcf415b6b773c8889968070ec5b9072
Andrey Tatarnikov <andrestat95@mail.ru>
2023-06-03 16:15:28 +0300

    add 'Items'

--- src/App.js
+++ src/App.js
@@ -5,23 +5,38 @@ function App() {
         <div className="drawer">
           <h2 className="mb-30">Корзина</h2>
 
-          <div className="cartItem d-flex align-center mb-20">
-            <div style={{ backgroundImage: 'url(/img/sneakers/1.jpg)'}} className="cartItemImg"></div>
-            <div className="mr-20 flex">
-              <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
-              <b>12 999 руб.</b>
+          <div className="Items">
+            <div className="cartItem d-flex align-center mb-20">
+              <div style={{ backgroundImage: 'url(/img/sneakers/1.jpg)'}} className="cartItemImg"></div>
+              <div className="mr-20 flex">
+                <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+                <b>12 999 руб.</b>
+              </div>
+              <img className="removeBtn" src="/img/sneakers/btn-rem1.svg" alt="Remove"></img>
             </div>
-            <img className="removeBtn" src="/img/sneakers/btn-rem1.svg" alt="Remove"></img>
+
+            <div className="cartItem d-flex align-center">
+              <div style={{ backgroundImage: 'url(/img/sneakers/1.jpg)'}} className="cartItemImg"></div>
+              <div className="mr-20 flex">
+                <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+                <b>12 999 руб.</b>
+              </div>
+              <img className="removeBtn" src="/img/sneakers/btn-rem1.svg" alt="Remove"></img>
+            </div>
           </div>
 
-          <div className="cartItem d-flex align-center">
-            <div style={{ backgroundImage: 'url(/img/sneakers/1.jpg)'}} className="cartItemImg"></div>
-            <div className="mr-20 flex">
-              <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
-              <b>12 999 руб.</b>
-            </div>
-            <img className="removeBtn" src="/img/sneakers/btn-rem1.svg" alt="Remove"></img>
-          </div>
+          <ul>
+            <li>
+              <span>Итого: </span>
+              <div></div>
+              <b>21 498 руб. </b>
+            </li>
+            <li>
+              <span>Налог 5%: </span>
+              <div></div>
+              <b>1074 руб. </b>
+            </li>
+          </ul>
         </div>
       </div>
 
