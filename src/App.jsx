c9c9015fa970cf0b6cd11953e83defea1cc00458
Andrey Tatarnikov <andrestat95@mail.ru>
2023-06-12 15:19:39 +0300

    add cartBlock

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,56 @@ function App() {
     <div className="wrapper clear">
       <div className="overlay">
         <div className="drawer">
-          <h2>Корзина</h2>
+          <h2 className="mb-30">Корзина</h2>
 
-          <div className="cartItem">
-            <img src="/img/sneakers/1.jpg" alt="Sneakers" />
-            <div>
-              <p>Мужские Кроссовки Nike Air Max 270</p>
-              <b>12 999 руб.</b>
+          <div className="items">
+            <div className="cartItem d-flex align-center mb-20">
+              <div
+                style={{ backgroundImage: "url(/img/sneakers/1.jpg)" }}
+                className="cartItemImg"
+              ></div>
+
+              <div className="mr-20 flex">
+                <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+                <b>12 999 руб.</b>
+              </div>
+              <img
+                className="RemoveBtn"
+                src="/img/sneakers/btn-rem1.svg"
+                alt="Remove"
+              />
+            </div>
+
+            <div className="cartItem d-flex align-center">
+              <div
+                style={{ backgroundImage: "url(/img/sneakers/1.jpg)" }}
+                className="cartItemImg"
+              ></div>
+
+              <div className="mr-20 flex">
+                <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+                <b>12 999 руб.</b>
+              </div>
+              <img
+                className="RemoveBtn"
+                src="/img/sneakers/btn-rem1.svg"
+                alt="Remove"
+              />
             </div>
           </div>
+
+          <ul className="cartTotalBlock">
+            <li>
+              <span>Итого: </span>
+              <div></div>
+              <b>21 498 руб. </b>
+            </li>
+            <li>
+              <span>Налог 5%: </span>
+              <div></div>
+              <b>1074 руб. </b>
+            </li>
+          </ul>
         </div>
       </div>
 
