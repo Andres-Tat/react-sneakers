d1ac9b5c09154c39080b2dc584671d08942a14e9
Andrey Tatarnikov <andrestat95@mail.ru>
2023-07-19 22:38:45 +0300

    add info in components 'Drawer'

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,12 @@
 import Card from "./components/Card";
 import Header from "./components/Header";
+import Drawer from "./components/Drawer";
 
 function App() {
   return (
     <div className="wrapper clear">
       <div style={{ display: "none" }} className="overlay">
-        <div className="drawer">
-          <h2 className="mb-30">Корзина</h2>
-
-          <div className="items flex">
-            <div className="cartItem d-flex align-center mb-20">
-              <div
-                style={{ backgroundImage: "url(/img/sneakers/1.jpg)" }}
-                className="cartItemImg"
-              ></div>
-
-              <div className="mr-20 flex">
-                <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
-                <b>12 999 руб.</b>
-              </div>
-              <img
-                className="RemoveBtn"
-                src="/img/sneakers/btn-rem1.svg"
-                alt="Remove"
-              />
-            </div>
-
-            <div className="cartItem d-flex align-center">
-              <div
-                style={{ backgroundImage: "url(/img/sneakers/1.jpg)" }}
-                className="cartItemImg"
-              ></div>
-
-              <div className="mr-20 flex">
-                <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
-                <b>12 999 руб.</b>
-              </div>
-              <img
-                className="RemoveBtn"
-                src="/img/sneakers/btn-rem1.svg"
-                alt="Remove"
-              />
-            </div>
-          </div>
-
-          <div className="cartTotalBlock">
-            <ul>
-              <li>
-                <span>Итого: </span>
-                <div></div>
-                <b>21 498 руб. </b>
-              </li>
-              <li>
-                <span>Налог 5%: </span>
-                <div></div>
-                <b>1074 руб. </b>
-              </li>
-            </ul>
-            <button className="greenButton">
-              Оформить заказ <img src="/img/sneakers/arrow.svg" alt="Arrow" />
-            </button>
-          </div>
-        </div>
+        <Drawer />
       </div>
       <Header />
       <div className="content p-40">
