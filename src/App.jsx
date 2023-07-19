0a2377b8b48871fcf725544047fdbc93a4100888
Andrey Tatarnikov <andrestat95@mail.ru>
2023-07-19 21:59:28 +0300

    create first 'components' 'card'

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import Card from "./components/Card";
+
 function App() {
   return (
     <div className="wrapper clear">
@@ -90,33 +92,7 @@ function App() {
         </div>
 
         <div className="d-flex">
-          <div className="card">
-            <div className="favourite">
-              <img src="/img/sneakers/heart-unliked.svg" alt="Unliked" />
-            </div>
-            <img
-              width={133}
-              height={112}
-              src="/img/sneakers/1.jpg"
-              alt="Sneakers"
-            ></img>
-            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
-            <div className="d-flex justify-between align-center">
-              <div className="d-flex flex-column">
-                <span>Цена:</span>
-                <b>12 999 руб.</b>
-              </div>
-              <button className="button">
-                <img
-                  width={11}
-                  height={11}
-                  src="/img/plus.svg"
-                  alt="Plus"
-                ></img>
-              </button>
-            </div>
-          </div>
-
+          <Card />
           <div className="card">
             <img
               width={133}
